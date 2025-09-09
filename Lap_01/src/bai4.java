824fb518a844064b24bf01dd1ef4f16f84cbc27f
duyan206 <leduyank34t1@gmail.com>
2025-09-09 22:47:12 +0700

    Update bai4.java

--- Lap_01/src/bai4.java
+++ Lap_01/src/bai4.java
@@ -3,24 +3,23 @@ import java.util.Scanner;
 public class bai4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap canh a: ");
         double a = sc.nextDouble();
+        System.out.println("Nhap canh b: ");
         double b = sc.nextDouble();
+        System.out.println("Nhap canh c: ");
         double c = sc.nextDouble();
 
         if (a + b > c && a + c > b && b + c > a){
-            // tinh chu vi
             double chuvi = a + b + c;
-
-            // nua chu vi
             double nuachuvi = chuvi / 2;
-
-            // dien tich
             double dientich = Math.sqrt(nuachuvi * (nuachuvi - a) * (nuachuvi - b) * (nuachuvi - c));
 
-            System.out.println(chuvi);
-            System.out.println(dientich);
-        }else {
-            System.out.println("khong tao thanh tam giac");
+            System.out.println("Chu vi la: " + chuvi);
+            System.out.println("Dien tich la: " + dientich);
+        }
+        else{
+            System.out.println("khong tao thanh tam giam");
         }
     }
 }
