7a5ec5c0585e80bf2f15914741645ff21658cd6e
duyan206 <leduyank34t1@gmail.com>
2025-09-09 22:48:46 +0700

    Update bai3.java

--- Lap_01/src/bai3.java
+++ Lap_01/src/bai3.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class bai3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        double dai = sc.nextDouble();
-        double rong = sc.nextDouble();
-
-        System.out.println("Chu Vi HCN: " + (2 * (dai + rong)) );
-        System.out.println("Dien Tich: " + (dai * rong) );
+        System.out.print("Nhap chieu dai: ");
+        int a = sc.nextInt();
+        System.out.println("Nhap chieu rong: ");
+        int b = sc.nextInt();
+        System.out.println("Chu vi hinh CN: " + (a+b)*2);
+        System.out.println("Dien tich hinh CN: " + (a*b));
     }
 }
